e29dea0685dd51218c377987d5f3153fb48aea1c
leolenis2018 <leolenis2018@gmail.com>
2021-09-07 08:59:29 -0500

    variasPantallas_2

--- App.js
+++ App.js
@@ -7,9 +7,13 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Button
 } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
+//export default function App() {
+function App(props) {
   const [contador, setContador] = useState(0);
   const onContar = () => {
     setContador((anterior) => anterior + 1);
@@ -35,7 +39,16 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <View style={{ flexDirection: "row", backgroundColor: "yellow" }}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            props.navigation.navigate("Somos", {
+              edad: 45,
+              nombre: "Hola desde el inicio",
+              salario: 2500000,
+            })
+          }
+        >
           <Text style={styles.text}>Quienes Somos</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
@@ -48,6 +61,68 @@ export default function App() {
     </View>
   );
 }
+class Somos extends React.Component {
+  /*Características de la pantalla como título - header*/
+  static navigationOptions = {
+    title: "Somos",
+    headerStyle: {
+      backgroundColor: "orange",
+    },
+    headerTintColor: "red",
+    headerTitleStyle: {
+      fontWeight: "bold",
+    },
+  };
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Somos</Text>
+        <Button
+          title="Ir a Inicio"
+          onPress={() => this.props.navigation.navigate("Inicio")}
+        />
+        <Text>
+          Edad:{" "}
+          {JSON.stringify(navigation.getParam("edad", "No se envió edad"))}
+          {/* itemId: {navigation.getParam('itemId', 'NO-ID')} */}
+        </Text>
+        <Text>
+          Nombre: {navigation.getParam("nombre", "")}
+          {/* otherParam: {JSON.stringify(navigation.getParam('otherParam', 'default value'))} */}
+        </Text>
+        <Text>
+          Salario: {navigation.getParam("salario", "0") * 2}
+        </Text>
+      </View>
+    );
+  }
+}
+// Pila de pantallas
+
+const RootStack = createStackNavigator(
+  {
+    Inicio: App,
+    Somos: Somos,
+    /*Contacts: Contactus,
+    Photos: Photos,
+    CUs: CUs,*/
+  },
+  {
+    initialRouteName: "Inicio",
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App1 extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+// fin pila de pantallas
 
 const styles = StyleSheet.create({
   container: {
