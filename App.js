4b6b0c2fb4bbddcc9009356a8c7c3e7236c09880
leolenis2018 <leolenis2018@gmail.com>
2021-09-07 08:03:45 -0500

    variasPantallas

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  const onContar = () => {
+    setContador((anterior) => anterior + 1);
+  };
+  const onDescontar = () => {
+    setContador((anterior) => anterior - 1);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text> Manejo de varias Pantallas</Text>
+      </View>
+      <View>
+        <Text> Contador: {contador}</Text>
+      </View>
+      <View style={{ flexDirection: "row", backgroundColor: "yellow" }}>
+        <TouchableOpacity style={styles.button} onPress={onContar}>
+          <Text style={styles.text}>Contar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={onDescontar}>
+          <Text style={styles.text}>Descontar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row", backgroundColor: "yellow" }}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Quienes Somos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Contáctenos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Grid</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +52,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  button: { margin: 5, backgroundColor: "aqua", padding: 5, borderRadius: 5 },
+  text: { fontWeight: "bold", textAlign: "center", color: "blue" },
 });
